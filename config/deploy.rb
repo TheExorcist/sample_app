9ff0d39c4e5601ae159222710981d6a1bc5a4b80
siddartha <siddartha.jangid@navyuginfo.com>
2015-11-05 19:09:08 +0530

    deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-puts 'hello'
+puts 'Deploy to aws'
 
 set :application, 'my_app_name'
 set :repo_url, 'git@example.com:me/my_repo.git'
